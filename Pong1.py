0e6888bf92a83f6ed1f64e845b25881dd9aa1218
2Tough <vainferro@gmail.com>
2022-12-05 16:04:33 +0300

    Update Pong1.py

--- Pong1.py
+++ Pong1.py
@@ -12,7 +12,11 @@ wn.tracer(0)
 
 # Paddle A
 paddle_a = turtle.Turtle()
-
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
 # Paddle B
 
