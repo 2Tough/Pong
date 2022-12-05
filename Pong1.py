15f695fb20bbc8b0e0fb77a28e55a18ad05e819c
2Tough <vainferro@gmail.com>
2022-12-05 15:59:41 +0300

    Create Pong1.py

---
+++ Pong1.py
@@ -0,0 +1,23 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Valentin")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+
+
+# Paddle A
+paddle_a = turtle.Turtle()
+
+
+# Paddle B
+
+# Ball
+
+# Main game loop
+while True:
+    wn.update()
